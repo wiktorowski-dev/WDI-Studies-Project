abc90cf0ce3da0b97c67938b1c86bbb9a513cc0d
Kamil <kamekw@gmail.com>
2020-10-25 07:39:52 +0100

    Requirements and setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='WDI-Studies-Project',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/wiktorowski-dev/WDI-Studies-Project',
+    license='MIT',
+    author='Kamil Wiktorowski',
+    author_email='wiktorowski.dev@gmail.com',
+    description=''
+)
